507b3926188e8472c09c10838891022969efc85c
PaulHaze <dev@artemist.com.au>
2022-10-01 09:53:57 +1000

    init commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+export function Home() {
+  return (
+    <div className="h-[100vh] bg-gradient-to-br from-teal-700/50 to-sky-500/50 flex items-center justify-center p-md">
+      <div className="flex flex-col bg-white rounded-lg shadow-2xl">
+        <h2 className="m-10 text-[#504e63] font-bold text-center">
+          Im your new React / Typescript Project
+        </h2>
+        <p className="text-[#504e63] text-center mb-10">
+          Delete this and make something great
+        </p>
+      </div>
+    </div>
+  );
+}
